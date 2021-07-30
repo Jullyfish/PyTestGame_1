f95aea5b03c04e06d9576354b24f7477f8a4c1a8
Jullyfish <88003527+Jullyfish@users.noreply.github.com>
2021-07-30 18:31:59 +0300

    Added terrible bullets

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pygame
 displayWidth, displayHeight, displayFrame = 1280, 720, 5
 
 pygame.init()
-win = pygame.display.set_mode((displayWidth, displayHeight), pygame.FULLSCREEN)
+win = pygame.display.set_mode((displayWidth, displayHeight)) #, pygame.FULLSCREEN
 
 pygame.display.set_caption('Test Game')
+
+
+#<sprites>
 
 walkRight = [
 pygame.image.load('sprites/R2.png'), pygame.image.load('sprites/R3.png'), pygame.image.load('sprites/R4.png'),
@@ -18,11 +21,14 @@ pygame.image.load('sprites/L5.png'), pygame.image.load('sprites/L6.png'), pygame
 pygame.image.load('sprites/L8.png'), pygame.image.load('sprites/L9.png'), pygame.image.load('sprites/L10.png')]
 
 jumpLeft = pygame.image.load('sprites/LJump.png')
+
 jumpRight = pygame.image.load('sprites/RJump.png')
 
 bg = pygame.image.load('backgrounds/bg1.jpg')
 playerStandRight = pygame.image.load('sprites/R0.png')
 playerStandLeft = pygame.image.load('sprites/L0.png')
+
+#</sprites>
 
 clock = pygame.time.Clock()
 
@@ -39,7 +45,7 @@ class player(object):
 		self.left = False
 		self.right = False
 		self.standRight = True
-		self.standLeft = False
+		self.standLeft = False		
 
 	def draw(self, win):
 		if sharpshooter.animCount + 1 >= len(walkRight) * 3:
@@ -63,15 +69,34 @@ class player(object):
 			win.blit(playerStandLeft, (sharpshooter.x, sharpshooter.y))	
 
 
+class projectile(object):
+	def __init__(self, x, y, radius, color, facing):
+		self.x = x
+		self.y = y			
+		self.radius = radius
+		self.color = color
+		self.facing = facing
+		if sharpshooter.standRight:
+			self.speed = 16
+		elif sharpshooter.standLeft:
+			self.speed = -16	
+
+	def	draw(self, win):
+		pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+
 def drawWindow():
 	win.blit(bg, (0, 0))
-	#pygame.draw.rect(win, (255, 0, 0), 
-	#(sharpshooter.x, sharpshooter.y, sharpshooter.width, sharpshooter.height)) #draw hitbox	
+	#pygame.draw.rect(win, (255, 255, 255), 
+	#(sharpshooter.x, sharpshooter.y, sharpshooter.width, sharpshooter.height), 2) #draw hitbox	
 	sharpshooter.draw(win) 
+	for bullet in bullets:
+		bullet.draw(win)
 	pygame.display.update()	
 
 
 sharpshooter = player(0, displayHeight - 240, 40, 80)
+bullets = []
 run = True
 while run:
 	clock.tick(30)
@@ -80,9 +105,20 @@ while run:
 		if event.type == pygame.QUIT:
 			run = False
 
+	for bullet in bullets:
+		if bullet.x < displayWidth and bullet.x > 0:
+			bullet.x += bullet.speed
+		else:
+			bullets.pop(bullets.index(bullet))	
+
 	keys = pygame.key.get_pressed()
+
 	if keys[pygame.K_ESCAPE]:
 		run = False		
+
+	if keys[pygame.K_s]:
+		bullets.append(projectile(round(sharpshooter.x + sharpshooter.width // 2), 
+		round(sharpshooter.y + sharpshooter.height // 2), 6, (0,0,0), 1))	
 
 	if keys[pygame.K_LEFT] and sharpshooter.x > displayFrame:
 		sharpshooter.x -= sharpshooter.speed		
@@ -112,5 +148,6 @@ while run:
 			sharpshooter.jumpCount = 10	
 
 	drawWindow()		
+
 
 pygame.quit()			
